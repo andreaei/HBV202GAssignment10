9412afdf029ab79a8ec56bf4914219253d633d85
Andrea E <ane28@hi.is>
2025-04-02 17:57:58 +0000

    changes for assignment 10

--- src/main/java/is/hi/hbv202g/ass5/IntStackMain.java
+++ src/main/java/is/hi/hbv202g/ass5/IntStackMain.java
@@ -1,6 +1,15 @@
 package is.hi.hbv202g.ass5;
 
+
+
 public class IntStackMain {
+
+    /** Main method that demonstrates stack operations.
+     * It creates a stack, checks if it's full, pushes to it, checks
+     * again if it's full, pops from it.
+     * @param args The command line arguments
+     * */
+
     public static void main(String[] args) {
         System.out.println("Creating a new stack");
         IntStack myStack = new IntStack();
